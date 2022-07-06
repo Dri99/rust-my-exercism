aa27f119ae40e08d25bfcf9d83ddccdf44efd6df
Alessandro Mandrile s296516 <s296516@studenti.polito.it>
2022-07-06 19:56:48 +0200

    optimized cdl on channel

--- exercises/countdown_latch_chan/src/lib.rs
+++ exercises/countdown_latch_chan/src/lib.rs
@@ -1,48 +1,37 @@
 use std::sync::{Mutex, Arc, mpsc};
 use std::sync::mpsc::{Receiver, Sender};
 
-
+#[derive(Clone)]
 pub struct CountDownLatch {
-    count : Arc<Mutex<u8>>,
-    locked: Arc<Mutex<u8>>,
+    count: Arc<Mutex<u8>>,
     receiver: Arc<Mutex<Receiver<()>>>,
-    sender: Sender<()>,
+    sender: Arc<Mutex<Option<Sender<()>>>>,
 }
 
 impl CountDownLatch {
     pub fn new(count: u8) -> Self {
         let (sender, receiver) = mpsc::channel::<()>();
         CountDownLatch {
-            count :Arc::new(Mutex::new(count)),
-            locked: Arc::new(Mutex::new(0)),
-            sender,
+            count: Arc::new(Mutex::new(count)),
+            sender: Arc::new(Mutex::new(Some(sender))),
             receiver: Arc::new(Mutex::new(receiver)),
         }
     }
 
     pub fn wait(&self) {
-
-        let mut locked = self.locked.lock().unwrap();
-        *locked += 1;
-        drop(locked);
-
-        let count = self.count.lock().unwrap();
-        if *count == 0 {
-            return;
-        }
-
         let rec = self.receiver.lock().unwrap();
-        rec.recv().unwrap();
+        rec.recv().unwrap_or(());
     }
 
     pub fn count_down(&self) {
         let mut count = self.count.lock().unwrap();
+        if *count == 0 {
+            return;
+        }
         *count -= 1;
         if *count == 0 {
-            let locked = self.locked.lock().unwrap();
-            for _ in 0..*locked {
-                self.sender.send(()).unwrap();
-            }
+            let mut sender_lock = self.sender.lock().unwrap();
+            *sender_lock = None;
         }
     }
 }
